a726bb65efcd2e37d44b61fbc806e0bc8c341dd0
Lilly <iminanavi1245@gmail.com>
2025-12-29 19:10:45 -0500

    add password verification

--- src/main.c
+++ src/main.c
@@ -2,9 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdbool.h>
 #include <crypt.h>
 
-void main(int argc, char **argv) {
+bool verify_password(const char* password, const char* hash) {
+    char *calculated_hash = crypt(password, hash);
+    // printf("Calculated hash: %s\n", calculated_hash);
+    if (calculated_hash == NULL) return false;
+    return strcmp(calculated_hash, hash) == 0;
+}
+
+int main(int argc, char **argv) {
     const char *password = "teehee";
 
     const char *salt = crypt_gensalt("$2a$", 12, NULL, 0);
@@ -17,8 +25,16 @@ void main(int argc, char **argv) {
         fprintf(stderr, "Error hashing password\n");
         exit(EXIT_FAILURE);
     }
+    hashed = strdup(hashed);
 
-    printf("Hashed password: %s\n", hashed);
+    // printf("Hashed password: %s\n", hashed);
+
+    if (verify_password("teehee", hashed)) {
+        printf("Password verified successfully.\n");
+    } else {
+        printf("Password verification failed.\n");
+    }
+
 
     return 0;
 }
